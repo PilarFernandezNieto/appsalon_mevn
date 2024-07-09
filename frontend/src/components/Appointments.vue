7bf400c3b63a439382c0d70aa301b5909cef9fac
PilarFernandezNieto <apreilmc@gmail.com>
2024-07-09 23:34:34 +0200

    Vista para editar

--- frontend/src/components/Appointments.vue
+++ frontend/src/components/Appointments.vue
@@ -24,7 +24,7 @@ defineProps({
         </p>
 
         <div class="flex gap-2 items-center">
-            <button class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">Editar Cita</button>
+            <RouterLink :to="{name: 'edit-appointment', params: {id: appointment._id}}" class="bg-slate-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">Editar Cita</RouterLink>
             <button class="bg-red-600 rounded-lg p-3 text-white text-sm uppercase font-black flex-1 md:flex-none">Cancelar Cita</button>
 
         </div>
---
+++ frontend/src/views/appointments/EditAppointmentLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { useRoute } from 'vue-router';
+const route = useRoute()
+
+</script>
+
+<template>
+    <h1 class="text-white">Desde editar</h1>
+    <nav class="my-5 flex gap-3">
+        <RouterLink :to="{ name: 'edit-appointment' }"
+            class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
+            :class="route.name === 'edit-appointment' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500' "
+            >Servicios
+        </RouterLink>
+        <RouterLink :to="{ name: 'edit-appointment-details' }"
+            class="flex-1 text-center p-3 uppercase font-extrabold hover:bg-blue-600 hover:text-white"
+            :class="route.name === 'edit-appointment-details' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500' "
+            >Cita y Resumen
+        </RouterLink>
+
+    </nav>
+    <RouterView />
+
+
+</template>
